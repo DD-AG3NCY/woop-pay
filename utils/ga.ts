5c3c55e2a1d500b49969d5060c0bfca94714ffeb
Alessandro Maci <alessandromaci96@gmail.com>
2022-12-21 14:25:34 +0100

    fixing vercel deploy issue
    (cherry picked from commit b6e3593f5a6244a4e71d139c5de0e39ed8caee55)

--- utils/ga.ts
+++ utils/ga.ts
@@ -2,13 +2,15 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: any) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if (GA_TRACKING_ID) {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }:any) => {
+export const event = ({ action, category, label, value }: any) => {
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
