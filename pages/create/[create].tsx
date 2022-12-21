0ace269dfe85a9c5ebea788fa54379d2b80b1e5b
Alessandro Maci <alessandromaci96@gmail.com>
2022-12-21 15:17:36 +0100

    Revert "redeploy vercel"
    This reverts commit 56c188a7cd23f89ee6fc22046c133bde7fd70097.

--- pages/create/[create].tsx
+++ pages/create/[create].tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Image from "next/image";
 import Head from "next/head";
 import { useRouter } from "next/router";
+
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Confetti from "react-confetti";
