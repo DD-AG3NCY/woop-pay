7de41ced7049e14b8bdff479d7e275a340981848
Alessandro Maci <alessandromaci96@gmail.com>
2023-01-19 17:38:04 +0100

    improving format for terms and privacy pages

--- pages/policy.tsx
+++ pages/policy.tsx
@@ -11,28 +11,25 @@ const Policy: React.FC<IPolicyProps> = (props) => {
 
   return (
     <article className="flex justify-center">
-      <div className="max-w-lg my-20">
-        <h2 className="font-semibold text-2xl mb-5">
+      <div className="max-w-lg my-20 p-5 rounded-lg shadow-md">
+        <h2 className="text-center font-semibold text-2xl mb-5">
           {"Woop Pay Privacy Policy"}
         </h2>
-        <p>{"Last modified: December 29, 2022"}</p>
-        <br />
-        <p>{"Data We Collect"}</p>
-        <p>
+        <p className="text-center mb-5">{"Last modified: December 29, 2022"}</p>
+        <p className="text-center mb-5">{"Data We Collect"}</p>
+        <p className="text-justify mb-5">
           {
             "Privacy is central to everything we do. Accordingly, we aspire to be transparent about the data we collect. We do not maintain user accounts and do not collect and store personal data, such as your name or internet protocol (“IP”) address. When you interact with the Services, we collect only publicly-available blockchain data and may also collect certain activity data using Google Analytics. When you connect your non-custodial blockchain wallet to the Services, we collect your publicly-available blockchain address. Note that blockchain addresses are publicly-available data that are not created or assigned by us or any central party, and by themselves are not personally identifying."
           }
         </p>
-        <br />
-        <p>{"How We Share Data"}</p>
-        <p>
+        <p className="text-center mb-5">{"How We Share Data"}</p>
+        <p className="text-justify mb-5">
           {
             "We may share or disclose the data we collect with service providers. We may share your information with our service providers to assist us in providing the Services. For example, we may share your wallet address with service providers like Infura to provide technical infrastructure services."
           }
         </p>
-        <br />
-        <p>{"Age Requirements"}</p>
-        <p>
+        <p className="text-center mb-5">{"Age Requirements"}</p>
+        <p className="text-justify mb-5">
           {
             "The Services are intended for a general audience and are not directed at children."
           }
