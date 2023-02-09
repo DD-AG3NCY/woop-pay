a965cccf18c582e1e774aee5947bdd118d773d47
alerex.eth <alessandromaci96@gmail.com>
2023-02-09 17:54:42 +0100

    adding style module to notification component

--- components/Notification/Notification.tsx
+++ components/Notification/Notification.tsx
@@ -1,14 +1,47 @@
+import * as React from "react";
 import { retrieveNotifications } from "../../utils/push";
 import { useAccount } from "wagmi";
+import styles from "./notification.module.scss";
 
 export default function Notification() {
+  const [showModal, setShowModal] = React.useState<boolean>(false);
   const { address } = useAccount();
+  const [notifications, setNotifications] = React.useState([]);
+
+  const retrieveData = async () => {
+    const data = await retrieveNotifications(address);
+    setNotifications(data);
+  };
+
+  React.useEffect(() => {
+    if (showModal) {
+      retrieveData();
+    }
+  }, [showModal]);
 
   return (
     <div>
-      <button type="button" onClick={() => retrieveNotifications(address)}>
+      <button type="button" onClick={() => setShowModal(true)}>
         See Notifications
       </button>
+      {showModal && (
+        <table className={styles.container}>
+          <thead>
+            <tr>
+              <th>Woop Pay Notifications</th>
+            </tr>
+          </thead>
+          <tbody>
+            {notifications.map((notification, index) => (
+              <tr key={index}>
+                <td>
+                  {notification?.title}: {notification?.message}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
