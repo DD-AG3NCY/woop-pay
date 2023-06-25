ed82b11ebe3d9beae89502ba0085d4e5e2eaf81a
alerex.eth <alessandromaci96@gmail.com>
2023-06-25 18:08:49 +0200

    Revert "display dashboard button if push subscription is active"
    This reverts commit 3ea0ac131ec19cb3fd240fc5ae2aff139b63b90a.

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { AiOutlineProfile } from "react-icons/ai";
-import { retrieveSubscriptions } from "../utils/push";
 import logo from "../public/logo.svg";
 import logoMobile from "../public/icon.svg";
 import Wallet from "./Wallet";
@@ -20,18 +19,10 @@ const Header: React.FC<IHeaderProps> = (props) => {
   const { address } = useAccount();
   const [showNotification, setShowNotification] =
     React.useState<boolean>(false);
-  const [isSubscribed, setIsSubscribed] = React.useState<boolean>(false);
-
-  const retrieveIsSubscribed = async () => {
-    const subs = await retrieveSubscriptions(address);
-    console.log(subs);
-    setIsSubscribed(subs);
-  };
 
   React.useEffect(() => {
     if (address) {
       setShowNotification(true);
-      retrieveIsSubscribed();
     } else {
       setShowNotification(false);
     }
@@ -68,7 +59,7 @@ const Header: React.FC<IHeaderProps> = (props) => {
                 "font-bold flex items-center"
               )}
             >
-              Dashboard
+              Dashboard <AiOutlineProfile className="ml-1" />
             </Link>
           </div>
         ) : (
