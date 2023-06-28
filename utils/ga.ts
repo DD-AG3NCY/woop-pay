be3009400e99a6e843baf64a83f3f4e5ce4ca54f
alerex.eth <alessandromaci96@gmail.com>
2023-06-28 17:30:50 +0200

    Revert "remove ga"
    This reverts commit d98644647d46c89c66be65f353fbaa66c480e1e8.

---
+++ utils/ga.ts
@@ -0,0 +1,26 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  if (GA_TRACKING_ID) {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
+};
+
+export interface IGaEvent {
+  action: string;
+  category: string;
+  label: string;
+  value: string;
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: IGaEvent) => {
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value
+  });
+};
