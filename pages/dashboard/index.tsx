b47422332b43de30f956af80c2baf812b1af351a
alerex.eth <alessandromaci96@gmail.com>
2023-09-18 09:09:59 +0300

    adjust size

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -90,7 +90,7 @@ const Dashboard = () => {
                     component="form"
                     className={cx(
                       styles.containerBoxNew,
-                      "rounded-3xl shadow-md relative z-20 p-4 mt-3 w-1/2 md:w-full"
+                      "rounded-3xl shadow-md relative z-20 p-4 mt-3 w-full md:w-1/2"
                     )}
                   >
                     <section className="text-center">
